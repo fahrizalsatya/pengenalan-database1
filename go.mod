f37b794d68da72671a4ec40547f0c6ede44ed419
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-07 02:54:14 +0700

    init skeleton for sql orm

--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/spf13/viper v1.7.1
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/gorm v1.20.5
 )
---
+++ sql-orm/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/FahrizalSatya/pengenalan-database1/sql-orm/config"
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_, err = initDB(cfg.Database)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+func getConfig() (config.Config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func initDB(dbConfig config.Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.DbName, dbConfig.Config)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("db successfully connected")
+
+	return db, nil
+}
